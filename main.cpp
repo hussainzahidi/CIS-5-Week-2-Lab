a1a2f479b1b7f4660770284da514d35d8b74e234
hussainzahidi <m.h.zahidi@outlook.com>
2026-09-09 21:28:51 -0700

    added my name and edited main.cpp file.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
+#include <string>
 
-// Lab 2 — Your Name
+// Lab 2 — Hussain Zahidi
 // CIS 5 Week 02 · Program structure
 
 int main() {
+	using std::string;
+	using std::cout;
+	using std::cin;
+
   // TODO: replace TODOs with YOUR labeled lines (5–6 total)
-  std::cout << "Name: TODO\n";
-  std::cout << "Interest: TODO\n";
-  std::cout << "CIS 5 goal: TODO\n";
-  std::cout << "Tool path: TODO\n";
-  std::cout << "Fun fact: TODO\n";
+	string name;
+	int age = 26;
+	cout << "Enter name: ";
+	cin >> name;
+	cout << "Name: " << "My name is " << name << ".\n";
+	cout << "Interest: I like to learn new recipes from different cuisines.\n";
+	cout << "CIS 5 goal: Understand programming logic through C++ and start an ai agent personal learning project!\n";
+	cout << "Tool path: I used Visual Studio 2022 with C++ build tools installed. I clone a repo from github and push through VS.\n";
+	cout << "Fun fact: I lived as an expat in Ras Tanura, Saudi Arabia for 10 years!\n";
+	cout << "Age: " << "I am " << age << " years old.\n";
   return 0;
 }
